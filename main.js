fb07b1d566ee0a8d2787e6f93ec93563817564b6
BabyDixyyy <105656112+BabyDixyyy@users.noreply.github.com>
2024-03-12 08:48:18 -0300

    ani

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
+
+for( let i = 0 <botoes.length; j++){
+    botoes[i].onclick = fuction (){
+        for (let k = 0; <botoes.length; j++)
+        botoes[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo")
+}
